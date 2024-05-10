95a5e687b9e8c1570342ef38b765eea32dbc5ae4
Jacob <jacobshan429@gmail.com>
2024-05-10 17:41:05 +0800

    chore: fix send script

--- AVAX-Teleporter/scripts/sendCrossChainMsg.js
+++ AVAX-Teleporter/scripts/sendCrossChainMsg.js
@@ -1,16 +1,16 @@
-const { ethers, getNamedAccounts, deployments } = require("hardhat");
+const { ethers, getNamedAccounts } = require("hardhat");
 
 async function main() {
   const { deployer } = await getNamedAccounts();
   const CChainSender = await ethers.getContractFactory("CChainSender");
   const cChainSender = CChainSender.attach("0x5201D8EF22bA3090eA3329d57F082801783A8558").connect(
-    await ethers.provider.getSigner(deployer)
+    ethers.provider.getSigner(deployer)
   );
 
   console.log("Sending message to DispatchReceiver from Fuji to dispatch");
   const sendToDispatchTxn = await cChainSender.sendMessage(
     "0x63681558c1b680E43bbCAdC0CeD21075854bBA87", // destination contract address
-    "chaining message" // message
+    "hello teleporter!" // message
   );
   await sendToDispatchTxn.wait();
   console.log("done");
